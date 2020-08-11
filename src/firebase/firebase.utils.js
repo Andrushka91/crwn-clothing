cd7e4bb8333f4367f75b091ac38c2d97af285d6a
andrei <rezmerita.andrey@gmail.com>
2020-08-11 12:48:29 +0200

    updated

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,13 +13,41 @@ const config = {
   measurementId: "G-SJN9KYFZ2B",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+return userRef;
+
+  console.log(snapShot);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({promt:'select_account'});
+provider.setCustomParameters({ promt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
